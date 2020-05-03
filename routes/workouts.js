0cacc462c5e380463ee0002551fec2b05899ac28
Lucidreline <ma52castaneda@gmail.com>
2020-05-02 20:16:04 -0700

    refactored each route to use await instal of call back functions

--- routes/workouts.js
+++ routes/workouts.js
@@ -3,117 +3,105 @@ const router = express.Router();
 
 const mongoose = require('mongoose');
 var workoutSchema = new mongoose.Schema({
-  name: String,
-  calories: Number,
-  durration: Number,
-  avgHeartRate: Number,
-  weight: Number,
-  image: String,
-  description: String,
-  date: { type: Date, default: Date.now },
-  distance: Number,
+    name: String,
+    calories: Number,
+    durration: Number,
+    avgHeartRate: Number,
+    weight: Number,
+    image: String,
+    description: String,
+    date: { type: Date, default: Date.now },
+    distance: Number,
 });
 
 var Workout = mongoose.model('Workout', workoutSchema);
+const defaultWorkoutImage =
+    'https://images.unsplash.com/photo-1517836477839-7072aaa8b121?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80';
 
 // @ route GET /workouts
 // @ desc renders the main workouts page
-router.get('/', function (req, res) {
-  Workout.find({})
-    .sort({ _id: -1 }) // switches the order so the newest workouts are on top of the list
-    .exec(function (err, workouts) {
-      if (err) {
+router.get('/', async (req, res) => {
+    try {
+        let workoutsList = await Workout.find({}).sort({ _id: -1 }).exec();
+        res.render('index', { workouts: workoutsList });
+    } catch (err) {
         console.log(err.message);
-      } else {
-        res.render('index', { workouts: workouts });
-      }
-    });
+        res.send('Unable to load workouts. Server Error');
+    }
 });
 
 // @ route GET /workouts/new
 // @ desc  renders the form to create a new workout
-router.get('/new', function (req, res) {
-  res.render('new');
-});
+router.get('/new', (req, res) => res.render('new'));
 
 // @ route POST /workouts
 // @ desc creates and adds a workout to the db
-router.post('/', function (req, res) {
-  Workout.create(req.body.workout, function (err, newWorkout) {
-    if (err) {
-      console.log('Could not create workout. Error: ' + err.message);
-    } else {
-      //add default photo if one is not given
-      if (newWorkout.image.trim() == '') {
-        newWorkout.image =
-          'https://images.unsplash.com/photo-1517836477839-7072aaa8b121?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80';
-        newWorkout.save((errorSaving, savedWorkout) => {
-          return;
-          res.redirect('/workouts');
-        });
-      }
-      res.redirect('/workouts');
+router.post('/', async (req, res) => {
+    try {
+        let newWorkout = await Workout.create(req.body.workout);
+
+        if (newWorkout.image.trim() == '') {
+            newWorkout.image = defaultWorkoutImage;
+            await newWorkout.save();
+        }
+    } catch (err) {
+        console.log('Could not create workout. Error: ' + err.message);
     }
-  });
+    res.redirect('/workouts');
 });
 
 // @ route GET /workouts/:id
 // @ desc  shows the workout's page with all it's details
-router.get('/:id', function (req, res) {
-  Workout.findById(req.params.id, function (err, foundWorkout) {
-    if (err) {
-      console.log(err.message);
-    } else {
-      res.render('show', { workout: foundWorkout });
+router.get('/:id', async (req, res) => {
+    try {
+        res.render('show', { workout: await Workout.findById(req.params.id) });
+    } catch (err) {
+        console.log(err.message);
+        res.redirect('/');
     }
-  });
 });
 
 // @ route GET /workouts/:id/edit
 // @ desc renders the form to edit a workout
-router.get('/:id/edit', function (req, res) {
-  Workout.findById(req.params.id, function (err, foundWorkout) {
-    if (err) {
-      console.log(err.message);
-    } else {
-      res.render('edit', { workout: foundWorkout });
+router.get('/:id/edit', async (req, res) => {
+    try {
+        res.render('edit', { workout: await Workout.findById(req.params.id) });
+    } catch (err) {
+        console.log(err.message);
+        res.redirect('/');
     }
-  });
 });
 
 // @ route PUT /workouts/:id
 // @ desc updates the workout in that database
-router.put('/:id', function (req, res) {
-  Workout.findByIdAndUpdate(req.params.id, req.body.workout, function (
-    err,
-    updatedWorkout
-  ) {
-    if (err) {
-      console.log(err.message);
-    } else {
-      if (updatedWorkout.image.trim() == '') {
-        updatedWorkout.image =
-          'https://images.unsplash.com/photo-1517836477839-7072aaa8b121?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80';
-        updatedWorkout.save((errorSaving, savedWorkout) => {
-          return;
-          res.redirect('/workouts/' + req.params.id);
-        });
-      }
-      res.redirect('/workouts/' + req.params.id);
+router.put('/:id', async (req, res) => {
+    try {
+        let updatedWorkout = await Workout.findByIdAndUpdate(
+            req.params.id,
+            req.body.workout
+        );
+
+        if (updatedWorkout.image.trim() == '') {
+            // gives the default image if they left the image url input empty
+            updatedWorkout.image = defaultWorkoutImage;
+            await updatedWorkout.save();
+        }
+        res.redirect('/workouts/' + req.params.id);
+    } catch (err) {
+        console.log(err.message);
+        res.redirect('/');
     }
-  });
 });
 
 // @ route DELETE /workouts/:id
 // @ desc
-router.delete('/:id', function (req, res) {
-  Workout.findByIdAndRemove(req.params.id, function (err) {
-    if (err) {
-      console.log(err.message);
-    } else {
-      res.redirect('/workouts');
+router.delete('/:id', async (req, res) => {
+    try {
+        await Workout.findByIdAndRemove(req.params.id);
+        res.redirect('/workouts');
+    } catch (err) {
+        console.log(err.message);
     }
-  });
 });
 
 module.exports = router;
